b3bb3b86cbb67a369246cdb2181dc70d8cf06ab4
Maggiexuiscute <your.maggiexuiscute@gmail.com>
2022-05-15 21:32:00 -0500

    change header

--- public/js/main.js
+++ public/js/main.js
@@ -103,3 +103,18 @@
 		setupSlideSize("m-slides-3");
 	}
 
+	window.addEventListener("scroll", function(e){
+		if(window.scrollY > 5) {
+			$("header").css("box-shadow", "8px 0 8px 0 rgba(0, 0, 0, 0.2)");
+			$("header").css("background-color", "white");
+			$(".ribbon").hide("slow");
+
+		}else{
+			$("header").css("box-shadow", "none");
+			if(screen.width > 560){
+				$("header").css("background-color", "rgba(0, 0, 0, 0)");
+			}
+			$(".ribbon").css("display","block");
+		}
+
+	}) ;
