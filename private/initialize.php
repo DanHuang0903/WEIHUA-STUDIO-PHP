8647296749ed720ec95fdd5bcfae1af015946fbd
Maggiexuiscute <your.maggiexuiscute@gmail.com>
2022-05-18 12:49:06 -0500

    test

--- private/initialize.php
+++ private/initialize.php
@@ -7,11 +7,12 @@
 	$current = $_SERVER['SCRIPT_NAME'];
 
 	$project_end = root($current);
-	
+	echo $project_end;
 	$project_root = substr($_SERVER['SCRIPT_NAME'], 0, $project_end);
 	define('WWW_ROOT', $project_root) ;
 	define('PUBLIC_PATH', WWW_ROOT . '/public');
 	define('PRIVATE_PATH', WWW_ROOT . '/private');
+	echo PUBLIC_PATH ;
 
 	$_AWARDS = 83;
 
