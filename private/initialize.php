1d0d11cf20cc9c771765458cb2145e36cf460f6b
Maggiexuiscute <your.maggiexuiscute@gmail.com>
2022-05-18 13:02:10 -0500

    change root

--- private/initialize.php
+++ private/initialize.php
@@ -5,18 +5,12 @@
 	define('PRIVATE_FOLDER', dirname(__FILE__));
 	define('PROJECT_PATH', PRIVATE_FOLDER);
 	$current = $_SERVER['SCRIPT_NAME'];
-	echo $current ;
-	echo "+" ;
-	$project_end = root($current);
-	echo $project_end ;
-	echo "+";
+	$project_end = strpos($current, '/public');
 	$project_root = substr($_SERVER['SCRIPT_NAME'], 0, $project_end);
-	echo $project_root ;
-	echo "+" ;
+	
 	define('WWW_ROOT', $project_root) ;
 	define('PUBLIC_PATH', WWW_ROOT . '/public');
 	define('PRIVATE_PATH', WWW_ROOT . '/private');
-	echo PUBLIC_PATH ;
 
 	$_AWARDS = 83;
 
